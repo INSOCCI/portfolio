6660ea51b4507fc8f460003edc39128660d0075a
인소현 <sohyun7305@naver.com>
2022-09-17 16:11:08 +0900

    Docs basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/purpose/NavBar";
+import AboutProjectPage from "./pages/AboutProjectPage";
+import MainPage from "./pages/MainPage";
+import ResumePage from "./pages/ResumePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+
+      <Routes>
+        <Route path="/*" element={<MainPage />} />
+        <Route path="/resume" element={<ResumePage />} />
+        <Route path="/projects/:projectname" element={<AboutProjectPage />} />
+      </Routes>
     </div>
   );
 }
